79893b59bc692d6630aecf2de202e1cd3acd1529
1R0NCL4D-B4ST10N <xanderwilliams22@augustana.edu>
2025-03-16 20:48:30 -0500

    Updated R File!

--- patientBilling/patientBillingTables.R
+++ patientBilling/patientBillingTables.R
@@ -18,17 +18,19 @@ df <- left_join(df_visit_patient, df_billing, by = 'VisitID')
 
 df$Month <- format(as.Date(df$VisitDate), "%B")
 
-ggplot(df, aes(x = Month, fill = Reason)) +
+ggplot(df, aes(x = Reason, fill = Month)) +
   geom_bar() +
   theme_minimal() +
-  labs(title = "Reason for Visit by Month", x = "Month", y = "Count", fill = "Reason")
+  labs(title = "Reason for Visit by Month", x = "Reason", y = "Count", fill = "Month") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-ggplot(df, aes(x = WalkIn, fill = Reason)) +
+ggplot(df, aes(x = Reason, fill = WalkIn)) +
   geom_bar(position = "dodge") +
   theme_minimal() +
-  labs(title = "Reason for Visit by Walk-In Status", x = "Walk-In?", y = "Count", fill = "Reason")
+  labs(title = "Reason for Visit by Walk-In Status", x = "Reason", y = "Count", fill = "Walk-In?") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-ggplot(df, aes(x = City, fill = Reason)) +
+ggplot(df, aes(x = Reason, fill = City)) +
   geom_bar(position = "stack") +
   theme_minimal() +
   labs(title = "Reason for Visit by City", x = "City", y = "Count", fill = "Reason") +
@@ -39,4 +41,11 @@ ggplot(df, aes(x = Reason, y = InvoiceAmt, fill = InvoicePaid)) +
   theme_minimal() +
   labs(title = "Total Invoice Amount by Reason for Visit", x = "Reason", y = "Total Invoice Amount", fill = "Invoice Paid?") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+ggplot(df, aes(x = Month, y = InvoiceAmt, fill = InvoicePaid)) +
+  geom_col() +
+  theme_minimal() +
+  labs(title = "Total Invoice Amount Per Month", x = "Month", y = "Total Invoice Amount", fill = "Invoice Paid?")
+
+
 
